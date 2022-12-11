cefa817adafe51b893cecaea4c96c798e32fd268
AMEXif <amex.if.email@seznam.cz>
2022-12-11 10:07:59 +0100

    fix colorBlendInfo self reference error for MSVC

--- src/include/pipeline.hpp
+++ src/include/pipeline.hpp
@@ -10,8 +10,6 @@ namespace Tyrotical {
         VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
         VkPipelineRasterizationStateCreateInfo rasterizationInfo;
         VkPipelineMultisampleStateCreateInfo multisampleInfo;
-        VkPipelineColorBlendAttachmentState colorBlendAttachment;
-        VkPipelineColorBlendStateCreateInfo colorBlendInfo;
         VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
         VkPipelineLayout pipelineLayout = nullptr;
         VkRenderPass renderPass = nullptr;
@@ -24,15 +22,15 @@ namespace Tyrotical {
         ~Pipeline();
 
         Pipeline(const Pipeline&) = delete;
-        void operator=(const Pipeline&) = delete;
+        Pipeline& operator=(const Pipeline&) = delete;
 
         static PipelineConfigInfo defaultConfig(uint32_t width, uint32_t height);
 
     private:
-        static char* fileContents(const char* filepath);
+        static std::vector<char> fileContents(const char* filepath);
 
         void createGraphicsPipeline(const PipelineConfigInfo& config, const char* vertexShaderPath, const char* fragmentShaderPath);
-        void createShaderModule(const char* code, VkShaderModule* shaderModule);
+        void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
         
         Device& _device;
         VkPipeline _pipeline;
--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -12,32 +12,31 @@ namespace Tyrotical {
         vkDestroyPipeline(_device.device(), _pipeline, nullptr);
     }
 
-    char* Pipeline::fileContents(const char* filepath) {
+    std::vector<char> Pipeline::fileContents(const char* filepath) {
         std::ifstream file{filepath, std::ios::ate | std::ios::binary};
 
         if (!file.is_open()) {
-            char* err;
-            sprintf(err, "[Error] failed to read file %s", filepath);
-            throw std::runtime_error(err);
-            free(err);
+            char* buf;
+            sprintf(buf, "failed to open file: %s", filepath);
+            throw std::runtime_error(buf);
         }
 
         size_t fileSize = static_cast<size_t>(file.tellg());
-        char* buf = new char[fileSize];
+        std::vector<char> buffer(fileSize);
 
         file.seekg(0);
-        file.read(buf, fileSize);
-        file.close();
+        file.read(buffer.data(), fileSize);
 
-        return buf;
+        file.close();
+        return buffer;
     }
 
     void Pipeline::createGraphicsPipeline(const PipelineConfigInfo& config, const char* vertexShaderPath, const char* fragmentShaderPath) {
         assert(config.pipelineLayout != VK_NULL_HANDLE && "[Error] Failed to create graphics pipeline: missing pipelineLayout in configInfo");
         assert(config.renderPass != VK_NULL_HANDLE && "[Error] Failed to create graphics pipeline: missing renderPass in configInfo");
         
-        char* vertexCode = fileContents(vertexShaderPath);
-        char* fragmentCode = fileContents(fragmentShaderPath);
+        auto vertexCode = fileContents(vertexShaderPath);
+        auto fragmentCode = fileContents(fragmentShaderPath);
 
         createShaderModule(vertexCode, &vertexShader);
         createShaderModule(fragmentCode, &fragmentShader);
@@ -60,7 +59,7 @@ namespace Tyrotical {
         shaderStages[1].pSpecializationInfo = nullptr;
 
         VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
-        vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
         vertexInputInfo.vertexAttributeDescriptionCount = 0;
         vertexInputInfo.vertexBindingDescriptionCount = 0;
         vertexInputInfo.pVertexAttributeDescriptions = nullptr;
@@ -73,6 +72,29 @@ namespace Tyrotical {
         viewportInfo.scissorCount = 1;
         viewportInfo.pScissors = &config.scissor;
 
+        VkPipelineColorBlendStateCreateInfo colorBlendInfo{};
+        VkPipelineColorBlendAttachmentState colorBlendAttachment{};
+        colorBlendAttachment.colorWriteMask =
+            VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT |
+            VK_COLOR_COMPONENT_A_BIT;
+        colorBlendAttachment.blendEnable = VK_FALSE;
+        colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;   // Optional
+        colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO;  // Optional
+        colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;              // Optional
+        colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;   // Optional
+        colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;  // Optional
+        colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;              // Optional
+    
+        colorBlendInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+        colorBlendInfo.logicOpEnable = VK_FALSE;
+        colorBlendInfo.logicOp = VK_LOGIC_OP_COPY;  // Optional
+        colorBlendInfo.attachmentCount = 1;
+        colorBlendInfo.pAttachments = &colorBlendAttachment;
+        colorBlendInfo.blendConstants[0] = 0.0f;  // Optional
+        colorBlendInfo.blendConstants[1] = 0.0f;  // Optional
+        colorBlendInfo.blendConstants[2] = 0.0f;  // Optional
+        colorBlendInfo.blendConstants[3] = 0.0f;  // Optional
+
         VkGraphicsPipelineCreateInfo pipelineInfo{};
         pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
         pipelineInfo.stageCount = 2;
@@ -82,7 +104,7 @@ namespace Tyrotical {
         pipelineInfo.pViewportState = &viewportInfo;
         pipelineInfo.pRasterizationState = &config.rasterizationInfo;
         pipelineInfo.pMultisampleState = &config.multisampleInfo;
-        pipelineInfo.pColorBlendState = &config.colorBlendInfo;
+        pipelineInfo.pColorBlendState = &colorBlendInfo;
         pipelineInfo.pDepthStencilState = &config.depthStencilInfo;
         pipelineInfo.pDynamicState = nullptr;
 
@@ -99,15 +121,14 @@ namespace Tyrotical {
         }
     }
 
-    void Pipeline::createShaderModule(const char* code, VkShaderModule* shaderModule) {
+    void Pipeline::createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule) {
         VkShaderModuleCreateInfo createInfo{};
-        const std::vector<char> codeArray(code, code + sizeof(code) / sizeof(*code));
         createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-        createInfo.codeSize = codeArray.size();
-        createInfo.pCode = reinterpret_cast<const uint32_t*>(codeArray.data());
+        createInfo.codeSize = code.size();
+        createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
 
         if (vkCreateShaderModule(_device.device(), &createInfo, nullptr, shaderModule) != VK_SUCCESS) {
-            throw std::runtime_error("[Error] Failed to create shader module");
+            throw std::runtime_error("failed to create shader module");
         }
     }
 
@@ -149,27 +170,6 @@ namespace Tyrotical {
         configInfo.multisampleInfo.pSampleMask = nullptr;             // Optional
         configInfo.multisampleInfo.alphaToCoverageEnable = VK_FALSE;  // Optional
         configInfo.multisampleInfo.alphaToOneEnable = VK_FALSE;       // Optional
-        
-        configInfo.colorBlendAttachment.colorWriteMask =
-            VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT |
-            VK_COLOR_COMPONENT_A_BIT;
-        configInfo.colorBlendAttachment.blendEnable = VK_FALSE;
-        configInfo.colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;   // Optional
-        configInfo.colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO;  // Optional
-        configInfo.colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;              // Optional
-        configInfo.colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;   // Optional
-        configInfo.colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;  // Optional
-        configInfo.colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;              // Optional
-        
-        configInfo.colorBlendInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
-        configInfo.colorBlendInfo.logicOpEnable = VK_FALSE;
-        configInfo.colorBlendInfo.logicOp = VK_LOGIC_OP_COPY;  // Optional
-        configInfo.colorBlendInfo.attachmentCount = 1;
-        configInfo.colorBlendInfo.pAttachments = &configInfo.colorBlendAttachment;
-        configInfo.colorBlendInfo.blendConstants[0] = 0.0f;  // Optional
-        configInfo.colorBlendInfo.blendConstants[1] = 0.0f;  // Optional
-        configInfo.colorBlendInfo.blendConstants[2] = 0.0f;  // Optional
-        configInfo.colorBlendInfo.blendConstants[3] = 0.0f;  // Optional
         
         configInfo.depthStencilInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
         configInfo.depthStencilInfo.depthTestEnable = VK_TRUE;
